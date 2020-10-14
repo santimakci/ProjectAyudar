eb2d1ec2cc79b9016aba931f70930adda7936584
Cremaschi, Agustín <cremaschi.agustin@gmail.com>
2020-10-14 20:23:47 -0300

    update y delete funcionando

--- app/models/user.py
+++ app/models/user.py
@@ -33,13 +33,11 @@ class User (base.Model):
     def find_by_email_and_pass(cls, mail, password):
         for user in base.session.query(User).filter(User.email==mail).filter(User.password==password).filter(User.deleted==False):
             return user         
-    
 
     @classmethod 
     def find_by_username(cls,username):
         for user in base.session.query(User).filter(User.username==username).filter(User.deleted==False):
             return user
-
 
     @classmethod
     def find_by_email(cls,email):
@@ -60,56 +58,59 @@ class User (base.Model):
         base.session.commit()
         return "Se creo el usuario perro"
     
-    @classmethod
+    
        #Acción de eliminar usuario.
        #Dejar disponible desde el listado la realización de esta acción. La acción requerirá confirmación para realizarse o para cancelarla.
        #Determinar estrategia de borrado (lógico o físico).
+    @classmethod
     def delete(self,params):
         user = self.find_by_id(params['id'])
         user.deleted=True
         user.date_deleted=base.func.now
         a = user.username
-      # base.session.commit()
+        base.session.commit()
         return f'se borro el usuario {a}'
         
+
+    """
     @classmethod
     def update(cls, params):
-        import code; code.interact(local=dict(globals(), **locals()))
-        """if (self.find_by_email(params['email']) or self.find_by_username(params['username'])): #Podríamos hacer una función por fuera no?
-           return "Email o username ya utilizado" """
         user_to_update = params # datos nuevos de el usuario a actualizar
         old_user = self.find_by_id(params['id']) # datos viejos del usuario a actualizar
         user_to_update.email = params['email']
-        if (equal_email(user_to_update,old_user) or equal_username(user_to_update.old_user)):
+        if (self.equal_email(user_to_update,old_user) or self.equal_username(user_to_update.old_user)):
            return "Email o username ya utilizado"
         user_to_update.password = params['password']
         user_to_update.first_name = params['first_name']
         user_to_update.last_name = params['last_name']
         base.session.commit()
-
-        # si se modifico el nombreusuario
-            #y no se encuentra disponible 
-                #return
-        # si se modifico el email =/ si mi email es dintinto 
-            #y no se encuentra disponible 
-                #return
-
-              #  another_user = self.self.find_by_email(params['email']) or self.find_by_username(params['username'])
-        #if another_user != None:
-
         return "Usuario actualizado correctamente"
-
+    """
 
     @classmethod
-    def equal_email(newUser, oldUser):
-        return (newUser.email == oldUser.email)
-    
-    @classmethod
-    def equal_username(newUser, oldUser):
-        return (newUser.username == oldUser.username)
-        
-         
+    def update(self,params):
 
-   
+        new_username = params['username'] 
+        new_password = params['password'] 
+        new_first_name = params['first_name']
+        new_last_name = params['last_name'] 
+        new_email = params['email']
 
+        old_user = self.find_by_id(params['id'])
 
+        if (new_username != old_user.username):
+            if(self.find_by_username(new_username)):
+                return "Email o username ya utilizado"
+
+        if (new_email != old_user.email):
+            if(self.find_by_email(new_email)):
+                return "Email o username ya utilizado"
+
+        #Si llegaste acá, significa que cambiaste el nombre o el email y que no había otro igual, asique todo piola
+        old_user.email = new_email
+        old_user.username = new_username
+        old_user.password = new_password
+        old_user.first_name = new_first_name
+        old_user.last_name = new_last_name
+        base.session.commit()
+        return "Usuario actualizado correctamente"
