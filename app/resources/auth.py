6ef73afedb8fab84805652bff80aeec66d245796
Vero Isoardi <vai.isoardi@gmail.com>
2020-10-29 00:00:05 -0300

    mantenimiento

--- app/resources/auth.py
+++ app/resources/auth.py
@@ -1,6 +1,7 @@
 from flask import redirect, render_template, request, url_for, abort, session, flash
 from app.db import connection
 from flask_session import Session
+from app.models.pageSetting import PageSetting
 from app.models.user import User
 from app.models.usersRoles import UsersRoles
 from app.models.rol import Rol
@@ -20,12 +21,18 @@ def authenticate():
     params = request.form
     elpass = hashlib.md5(params['password'].encode('utf-8')).hexdigest()
     user = User.find_by_email_and_pass(params["email"], elpass)
+    settings = PageSetting.find_settings()
     if not user:
         flash("Usuario o clave incorrecto.", 'danger')
         return redirect(url_for("auth_login"))
+    else:
+        user_roles = UsersRoles.find_user_roles_by_id(user.id)
+        if not (settings.enabled):
+            if 1 not in user_roles: 
+                flash("El sitio está en mantenimiento, intente de nuevo más tarde", 'danger')
+                return redirect(url_for("home"))                         
     session["user"] = user.username
     session['id'] = user.id
-    user_roles = UsersRoles.find_user_roles_by_id(user.id)
     session['roles'] = Rol.get_name_roles(user_roles)
     return redirect(url_for("home"))
 
