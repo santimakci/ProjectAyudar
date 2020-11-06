0eacf9fe96a337145e0ece2491e65644e09f1e6d
Santiago <santimakcimovich@gmail.com>
2020-11-06 17:40:03 -0300

    Api center paginada

--- app/resources/api/center.py
+++ app/resources/api/center.py
@@ -1,13 +1,36 @@
 from flask import jsonify
 from app.models.center import Center
-from flask import request
+from flask import request, render_template
+from app.models.pageSetting import PageSetting
 import json
 
 
 def centers():
     if request.method == "GET":
         centros = Center.return_centers_API_Data()
-        return jsonify(centers=centros)
+        count = len(centros)
+        start = int(request.args.get('page'))
+        limit = (PageSetting.find_settings()).elements
+        if ((count/limit) < start):    
+            return render_template("errors/error404.html")
+        obj = {}
+        obj['page'] = start
+        obj['limit'] = limit
+        obj['count'] = count
+        if start == 1:
+            obj['previous'] = ''
+        else:
+            start_copy = max(1, start - limit)
+            obj['previous'] = 'localhost:5000/centros' + '?page=%d' % (start_copy)
+        # make next url
+        if start + limit > count:
+            obj['next'] = ''
+        else:
+            start_copy = start + limit
+            obj['next'] = 'localhost:5000/centros' + '?page=%d' % (start_copy)
+        # finally extract result according to bounds
+        obj['centros'] = centros[(start - 1):(start - 1 + limit)]
+        return jsonify(obj)
     else:
         params = json.loads(request.data)
         mensaje = Center.create(params)
