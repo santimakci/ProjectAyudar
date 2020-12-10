77b6be26e5bbcd2c7595c61e4cd761c5a8d5e49e
Gastón Ginestet <gastonauginestet@gmail.com>
2020-12-10 15:32:19 -0300

    mejoras en el formulario

--- web/src/components/AddTurn.vue
+++ web/src/components/AddTurn.vue
@@ -57,14 +57,47 @@
                 cols="12"
                 sm="6"
               >
-              <v-select
-                v-model="turn.day"
-                :items="available_days"
-                label="Fecha"
-                required
-                @input="$v.turn.time.$touch()"
-                @blur="$v.center.time.$touch()"
-              ></v-select>
+                <v-menu
+                  ref="menu"
+                  v-model="menu"
+                  :close-on-content-click="false"
+                  :return-value.sync="date"
+                  transition="scale-transition"
+                  offset-y
+                  min-width="290px"
+                >
+                  <template v-slot:activator="{ on, attrs }">
+                    <v-text-field
+                      v-model="date"
+                      label="Fecha"
+                      prepend-icon="mdi-calendar"
+                      readonly
+                      v-bind="attrs"
+                      v-on="on"
+                    ></v-text-field>
+                  </template>
+                  <v-date-picker
+                    v-model="date"
+                    no-title
+                    scrollable
+                  >
+                    <v-spacer></v-spacer>
+                    <v-btn
+                      text
+                      color="primary"
+                      @click="menu = false"
+                    >
+                      Cancel
+                    </v-btn>
+                    <v-btn
+                      text
+                      color="primary"
+                      @click="$refs.menu.save(date)"
+                    >
+                      OK
+                    </v-btn>
+                  </v-date-picker>
+                </v-menu>
               </v-col>
               <v-col
                 cols="12"
